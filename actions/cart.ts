9f2854f365c41ec222a3af1eabbfa46acf6a348a
ShriramShanbhag <shriramshanbhag@hotmail.com>
2026-05-19 21:16:41 +0530

    removed async inside promise

--- actions/cart.ts
+++ actions/cart.ts
@@ -1,7 +1,7 @@
 'use server'
 export async function addToCart(productId: String) {
-    return new Promise(async (resolve, reject) => {
-        await setTimeout(() => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
             console.log(`Adding ${productId} to cart...`)
             resolve({status: 200})
         }, 5000)
