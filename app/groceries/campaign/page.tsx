d8e9a9d4f1b2caadb02abf945f7c4dcdd0827a03
ShriramShanbhag <shriramshanbhag@hotmail.com>
2026-05-19 20:23:41 +0530

    [Solution] Problem 4 - Core Web Vitals

--- app/groceries/campaign/page.tsx
+++ app/groceries/campaign/page.tsx
@@ -1,29 +1,36 @@
 // app/groceries/campaign/page.tsx
+import { Playfair_Display } from "next/font/google";
+import Image from "next/image";
+
+const playfair = Playfair_Display({
+  subsets: ["latin"],
+  style: ["italic"],
+  display: "swap",
+});
 
 export default function CampaignPage() {
   return (
     <main className="p-8">
-      {/* 1. The Font Issue */}
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital@1&display=swap" rel="stylesheet" />
-      </head>
 
-      <h1 className="text-4xl" style={{ fontFamily: "'Playfair Display', serif" }}>
+      <h1 className={`${playfair.className} text-4xl`} >
         Summer Fresh Produce
       </h1>
 
-      <p className="mt-4 mb-8 text-gray-700">
-        Discover our hand-picked selection of summer fruits, sourced directly from local farms.
-      </p>
-
       {/* 2. The Image Issue (Massive 5MB image from an external CMS) */}
       <div className="campaign-banner">
-        <img 
+        <Image 
           src="https://svs.gsfc.nasa.gov/vis/a020000/a020300/a020326/frames/3840x2160_16x9_30p/Slew_to_Pegasus_Attitude/OrexModel.115728.png" 
           alt="Fresh summer fruits basket" 
+          width={1200}
+          height={800}
+          unoptimized
+          priority
         />
       </div>
+
+       <p className="mt-4 mb-8 text-gray-700">
+        Discover our hand-picked selection of summer fruits, sourced directly from local farms.
+      </p>
 
     </main>
   );
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'svs.gsfc.nasa.gov',
+        protocol: 'https',
+        pathname: '/**'
+
+      }
+    ]
+  }
 };
 
 export default nextConfig;
